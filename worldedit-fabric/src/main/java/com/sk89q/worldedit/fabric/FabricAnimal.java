10e426e2fe0dc5ff1ebff39909aaa805ab7739ab
Patrícia <pg.costa@campus.fct.unl.pt>
2024-11-27 19:13:06 +0000

    changed the way to set cat and frog variants.

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricAnimal.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricAnimal.java
@@ -1,6 +1,10 @@
 package com.sk89q.worldedit.fabric;
 
 import com.sk89q.worldedit.fabric.internal.FabricEntity;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
 import net.minecraft.world.entity.animal.frog.Frog;
@@ -34,22 +38,38 @@ public class FabricAnimal extends FabricEntity implements com.sk89q.worldedit.wo
     }
 
     @Override
-    public void setVariant(String variant) {
-        if (variant == null)
+    public void setVariant(String variantID) {
+        if (variantID == null)
             return;
         switch (animal) {
-            case Parrot parrot -> parrot.setVariant(Parrot.Variant.valueOf(variant.toUpperCase()));
-            case Axolotl axolotl -> axolotl.setVariant(Axolotl.Variant.valueOf(variant.toUpperCase()));
-            case Fox fox -> fox.setVariant(Fox.Type.valueOf(variant.toUpperCase()));
-            case Horse horse -> horse.setVariant(Variant.valueOf(variant.toUpperCase()));
-            case Llama llama -> llama.setVariant(Llama.Variant.valueOf(variant.toUpperCase()));
+            case Parrot parrot -> parrot.setVariant(Parrot.Variant.valueOf(variantID.toUpperCase()));
+            case Axolotl axolotl -> axolotl.setVariant(Axolotl.Variant.valueOf(variantID.toUpperCase()));
+            case Fox fox -> fox.setVariant(Fox.Type.valueOf(variantID.toUpperCase()));
+            case Horse horse -> horse.setVariant(Variant.valueOf(variantID.toUpperCase()));
+            case Llama llama -> llama.setVariant(Llama.Variant.valueOf(variantID.toUpperCase()));
             case MushroomCow mushroomCow ->
-                    mushroomCow.setVariant(MushroomCow.MushroomType.valueOf(variant.toUpperCase()));
-            case Rabbit rabbit -> rabbit.setVariant(Rabbit.Variant.valueOf(variant.toUpperCase()));
-            case Frog frog -> FabricFrogVariantChanger.setVariant(frog, variant);
-            case Cat cat -> FabricCatVariantChanger.setVariant(cat, variant);
+                    mushroomCow.setVariant(MushroomCow.MushroomType.valueOf(variantID.toUpperCase()));
+            case Rabbit rabbit -> rabbit.setVariant(Rabbit.Variant.valueOf(variantID.toUpperCase()));
+            case Frog frog -> frog.setVariant(getFrogVariant(variantID));
+            case Cat cat -> cat.setVariant(getCatVariant(variantID));
             default -> throw new IllegalArgumentException();
         }
-        this.variant = variant;
+        this.variant = variantID;
+    }
+
+    private Holder<CatVariant> getCatVariant(String variantID) {
+        Registry<CatVariant> catVariantRegistry = FabricWorldEdit.getRegistry(Registries.CAT_VARIANT);
+        CatVariant catV = catVariantRegistry.get(ResourceLocation.parse("minecraft:" + variantID));
+        if (catV == null)
+            throw new IllegalArgumentException();
+        return Holder.direct(catV);
+    }
+
+    private Holder<FrogVariant> getFrogVariant(String variantID) {
+        Registry<FrogVariant> frogVariantRegistry = FabricWorldEdit.getRegistry(Registries.FROG_VARIANT);
+        FrogVariant frogV = frogVariantRegistry.get(ResourceLocation.parse("minecraft:" + variantID));
+        if (frogV == null)
+            throw new IllegalArgumentException();
+        return Holder.direct(frogV);
     }
 }
